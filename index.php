abbfedefe37c58305761699820ba464a522e19d8
vhtam <vo.hoai.tam@quantic.com.vn>
2021-01-06 12:20:28 +0700

    Update

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
-<?php 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
 
-require("vendor/autoload.php");
+    require("vendor/autoload.php");
 
-if(isset($_GET['song'])) {
+    use GuzzleHttp\Client;
 
-    var_dump($_GET['song']);
-}
+    $listSong = null;
+
+    if(isset($_POST['song'])) {
+        $zing = new Zing();
+        $msg = $_POST['song'];
+        $listSong = $zing->searchSong($msg);
+    }
+    ?>
+
+    <form action="" method="POST">
+        <input type="text" name="song" value="<?= (isset($_POST['song'])) ? $_POST['song'] : '' ?>" placeholder="Tìm tên bài hát ....">
+        <button type="submit">Tìm</button>
+    </form>
+
+    <br>
+    <?php if(!empty($listSong)) { ?>
+    <ul>
+        <?php foreach ($listSong as $key => $value) {
+        } ?>
+            <li>
+                <b><?= $value['title']?></b> - <i><?= $value['singer']?></i> - <a href="<?= $value['link'] ?>">Link</a>
+            </li>
+        <?php } ?>
+    </ul>
+    <?php } ?>
+</body>
+</html>
