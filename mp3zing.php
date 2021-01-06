ec53923a39f535db54d50b121f94fb14a53300e8
vhtam <vo.hoai.tam@quantic.com.vn>
2021-01-06 11:37:58 +0700

    INIT

---
+++ mp3zing.php
@@ -0,0 +1,161 @@
+<?php 
+
+require("vendor/autoload.php");
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+
+class Zing {
+
+    private $baseURL = "https://zingmp3.vn";
+    private $limitSong = 10;
+    private $version =  "1.0.16";
+    private $apiKey = "kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw";
+    private $secretKey = "882QcNXV4tUZbvAsjmFOHqNC1LpcBRKW";
+    private $cookie = null;
+
+    // "882QcNXV4tUZbvAsjmFOHqNC1LpcBRKW"
+
+    private $endPoint = [
+        "search" => "/api/v2/search/multi",
+        // "song" => "/api/song/get-song-info"
+        "song" => "/api/v2/song/getStreaming"
+
+
+    ];
+
+    public function hash256($str){
+        return hash('sha256', $str);
+    }
+
+    public function hash512($str, $key){
+        return hash_hmac("sha512", $str, $key);
+    }
+
+    public function getSig($url, $str, $key) {
+        return $this->hash512($url.$this->hash256($str), $key);
+    }
+
+    public function searchSong($songName, $page = 2) {
+        $client = new Client(['verify' => false]);
+
+        $cookieJar = CookieJar::fromArray($this->getCookies(), 'zingmp3.vn');
+
+        $listSong = [];
+
+        $ctime = strtotime('now');
+
+        $str = "ctime={$ctime}page={$page}type=songversion={$this->version}";
+        
+        $sig = $this->getSig($this->endPoint['search'], $str, $this->secretKey);
+
+        $urlParams = http_build_query(
+            [
+                "q" => $songName,
+                "type" => "song",
+                "page" => $page,
+                "ctime" => $ctime,
+                "version" => $this->version,
+                "sig" => $sig,
+                "apiKey" => $this->apiKey
+            ]
+        );
+
+        $url = $this->baseURL.$this->endPoint['search']."?".$urlParams;
+        
+        $res = $client->request("GET", $url, [
+            "headers" => [
+                "Host" => "zingmp3.vn"                
+            ],
+            'cookies' => $cookieJar
+        ]);
+
+
+        $arrData = json_decode($res->getBody()->getContents());
+
+        $listSong = [];
+        
+        foreach ($arrData->data->songs as $value) {
+            if (is_array($value) || is_object($value)) {
+                array_push($listSong, 
+                    [
+                        "id" => $value->encodeId,
+                        "title" => $value->title,
+                        "singer" => $value->artistsNames,
+                        "link" => $this->songDetail($value->encodeId)
+                    ]
+                );
+            }
+        }
+
+        return $listSong;
+    }
+
+    public function getCookies() {
+        $client = new Client(['verify' => false, 'cookies' => true]);         
+
+        $res = $client->request("GET", "https://zingmp3.vn/",[]);
+        $arrData = $client->getConfig('cookies')->toArray();
+        $cookies = [];
+        foreach ($arrData as $key => $value) {
+            $cookies[$value['Name']] = $value["Value"];
+        }
+        return $cookies;
+    }
+
+    public function songDetail($songID) {
+        $songID = "ZOZ0WD80";
+
+        $client = new Client(['verify' => false]);
+
+        $cookieJar = CookieJar::fromArray($this->getCookies(), 'zingmp3.vn');
+
+        $ctime = strtotime('now');
+        // $ctime = 1609841751;
+
+        $str = "ctime={$ctime}id={$songID}version={$this->version}";
+
+        $sig = $this->getSig($this->endPoint['song'], $str, $this->secretKey);
+
+        $urlParams = http_build_query(
+            [
+                "id" => $songID,
+                "ctime" => $ctime,
+                "version" => $this->version,
+                "sig" => $sig,
+                "apiKey" => $this->apiKey
+            ]
+        );
+
+       $url = $this->baseURL.$this->endPoint['song']."?".$urlParams;
+
+        $res = $client->request("GET", $url, [
+            "headers" => [
+                "Host" => "zingmp3.vn",
+            ],
+            'cookies' => $cookieJar
+        ]);
+
+        $arrData = json_decode($res->getBody()->getContents());
+        
+        return $arrData->data->{'128'};
+    }
+}
+
+
+
+// const crypto = require('crypto');
+
+
+// const getHash256 = (a) => {
+//     return crypto.createHash('sha256').update(a).digest('hex');
+// }
+// const getHmac512 = (str, key) => {
+//     let hmac = crypto.createHmac("sha512", key);
+//     return hmac.update(Buffer.from(str, 'utf8')).digest("hex");
+// }
+
+// var str = "ctime=160718421id=ZOW0OBU8";
+// var hash256 = getHash256(str);
+
+// console.log(getHmac512("/song/get-song-info"+hash256, "10a01dcf33762d3a204cb96429918ff6"));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,135 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e165677ac330c3fb68112f564942c82dad3fa0a0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e165677ac330c3fb68112f564942c82dad3fa0a0',
+    ),
+    'composer/package-versions-deprecated' => 
+    array (
+      'pretty_version' => '1.11.99.1',
+      'version' => '1.11.99.1',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7413f0b55a051e89485c5cb9f765fe24bb02a7b6',
+    ),
+    'doctrine/cache' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13e3381b25847283a91948d04640543941309727',
+    ),
+    'doctrine/dbal' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ee6d1260d5cc20ec506455a585945d7bdb98662c',
+    ),
+    'doctrine/event-manager' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '41370af6a30faa9dc0368c4a6814d596e81aba7f',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.2.0',
+      'version' => '7.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0aa74dfb41ae110835923ef10a9d803a22d50e79',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60d379c243457e073cff02bc323a2a86cb355631',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53330f47520498c0ae1f61f7e2c90f55690c06a3',
+    ),
+    'ocramius/package-versions' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '1.11.99',
+      ),
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+  ),
+);
